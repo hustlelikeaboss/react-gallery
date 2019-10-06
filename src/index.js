86c53e06b522d2a233e927b937511fe97a4cde68
David Q. Jin <quan.jin@me.com>
2019-10-06 12:49:17 -0500

    display photos from unsplash in a list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
+import Unsplash from 'unsplash-js';
 
 import { GridGallery } from './lib';
 import './App.css';
 
-const App = () => (
-	<div>
-		<h1>React Gallery</h1>
+const unsplash = new Unsplash({
+	applicationId: process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
+	secret: process.env.REACT_APP_UNSPLASH_SECRET_KEY
+});
 
-		<h2>Grid</h2>
-		<GridGallery />
-	</div>
-);
+const App = () => {
+	const [photos, setPhotos] = useState([]);
+	useEffect(() => {
+		unsplash.collections
+			.getCollectionPhotos(1248080, 1)
+			.then(res => res.json())
+			.then(json => {
+				setPhotos(json);
+			});
+	}, []);
+	return (
+		<div>
+			<h1>React Gallery</h1>
+
+			<h2>Grid</h2>
+			<GridGallery items={photos} />
+		</div>
+	);
+};
 
 render(<App />, document.getElementById('root'));
--- src/lib/GridGallery.jsx
+++ src/lib/GridGallery.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-export default function GridGallery(props) {
-	return <div>Example</div>;
-}
+const GridGallery = React.memo(({ items, render }) => {
+	return (
+		<div>
+			{items.map(
+				render ||
+					(item => (
+						<img
+							key={item.id}
+							src={item.urls.regular}
+							alt={item.description}
+						/>
+					))
+			)}
+		</div>
+	);
+});
+
+export default GridGallery;
