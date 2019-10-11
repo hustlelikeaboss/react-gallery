84c8f4f084abcb43622ca2e45616a2c246b61a34
David Q. Jin <quan.jin@me.com>
2019-10-11 13:00:39 -0500

    use flexbox to make footer stick to the bottom when main content doesn't take up the entire viewport height

--- src/examples/Footer.tsx
+++ src/examples/Footer.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
 
 export default function Footer() {
-	return (
-		<p
-			style={{
-				textAlign: 'center',
-				margin: '3rem 0 2rem 0'
-			}}
-		>
-			&copy; 2019 Hustle Like A Boss Design
-		</p>
-	);
+	return <footer>&copy; 2019 Hustle Like A Boss Design</footer>;
 }
