756a036a44894e5e875bd4f167ae6a000a864f0b
David Q. Jin <quan.jin@me.com>
2019-10-11 11:49:17 -0500

    removed logs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
 function itemsToCols(items: Array<any>, itemsPerRow = 4): Array<any> {
 	const numOfCols = Math.ceil(items.length / itemsPerRow);
-	console.log('numOfCols: ', numOfCols);
-
 	let cols = [];
 	for (let col = 0; col < numOfCols - 1; col++) {
 		cols.push(
@@ -9,7 +7,6 @@ function itemsToCols(items: Array<any>, itemsPerRow = 4): Array<any> {
 		);
 	}
 	cols.push(items.slice((numOfCols - 1) * itemsPerRow, items.length));
-	console.log('cols: ', cols);
 	return cols;
 }
 
